d5779c7c2beef89f6f77ff1ffc4ec91410220f25
quil <quil@quil-arch.localhost>
2023-10-01 06:12:42 +0000

    Create file flag and add Test dependencies

--- quil_qol/main.py
+++ quil_qol/main.py
@@ -7,11 +7,21 @@ app = typer.Typer()
 
 @app.command()
 def compare(
-    string1: Annotated[str, typer.Argument(help="First string to compare")],
-    string2: Annotated[str, typer.Argument(help="Second string to compare")],
+    string1: Annotated[str, typer.Argument(help="First string to compare")] = "",
+    string2: Annotated[str, typer.Argument(help="Second string to compare")] = "",
+    file: Annotated[
+        bool,
+        typer.Option("--file", "-f", help="Compare with the content of a file"),
+    ] = False,
 ):
     """Compare two strings and print the result"""
+    if file:
+        with open(string2, "r") as f:
+            string2 = f.read()
     if string1 == string2:
-        print("The strings are the same")
+        if file:
+            print("The strings are the same and the file flag is set")
+        else:
+            print("The strings are the same")
     else:
         print("The strings are different")
